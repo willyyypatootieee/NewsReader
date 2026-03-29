4dfe83bff14eb95c2f89f64e81c3c215735cc8fa
willyyypatootieee <blueplayz26@gmail.com>
2026-03-30 05:37:15 +0700

    feat: Integrate NewsViewModel and NewsScreen into MainActivity

--- app/src/main/java/com/example/mandirisubmission/MainActivity.kt
+++ app/src/main/java/com/example/mandirisubmission/MainActivity.kt
@@ -3,45 +3,22 @@ package com.example.mandirisubmission
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mandirisubmission.ui.theme.MandiriSubmissionTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mandirisubmission.ui.screen.NewsScreen
+import com.example.mandirisubmission.viewmodel.NewsViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MandiriSubmissionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Surface {
+                    val newsViewModel: NewsViewModel = viewModel()
+                    NewsScreen(viewModel = newsViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MandiriSubmissionTheme {
-        Greeting("Android")
     }
 }
